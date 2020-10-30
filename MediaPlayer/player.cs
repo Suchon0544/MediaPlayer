25e4b6446ffcc031752e092d8d59ef91a2d22b79
suchon <57866286+Suchon0544@users.noreply.github.com>
2020-10-30 12:38:15 +0700

    Add files via upload

---
+++ MediaPlayer/player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer
+{
+    class Player
+    {
+        public void player_display()
+        {
+
+            Console.WriteLine("Input: p for play your music");
+            Console.WriteLine("       n for play next music");
+            Console.WriteLine("       r for play previous music");
+            Console.WriteLine("       s for stop playing");
+            Console.WriteLine("       a for add music");
+            Console.WriteLine("       q for show tracklist");
+            Console.WriteLine("       e for close");
+            Console.WriteLine("     ");
+            Console.WriteLine("Now Playing   :  --");
+            Console.WriteLine("Artist   :  --");
+            Console.WriteLine(" --.-- ------------------------- --.--");
+            Console.WriteLine("<               [play]               >");
+        }
+    }
+}
