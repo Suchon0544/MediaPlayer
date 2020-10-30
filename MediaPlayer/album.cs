d4d27023086e8cf9fadde5fa4ac3c15342f9835f
suchon <57866286+Suchon0544@users.noreply.github.com>
2020-10-30 10:39:35 +0700

    Update album.cs

--- MediaPlayer/album.cs
+++ MediaPlayer/album.cs
@@ -8,42 +8,38 @@ namespace MediaPlayer
 {
     public class Album
     {
-        public Track[] tracks;
-        //public int TrackList { get; }
-        public int Tracklist = 0;
+        public List<Track> tracks = new List<Track>();
+        int Tracklist;
         public Album()
         {
-            tracks = new Track[10];
-            for (int i = 0; i < tracks.Length; i++)
-            {
-                tracks[i] = new Track();
-            }
 
+            tracks.Add(new Track("Camelia", "MILI", "04.43"));
+            tracks.Add(new Track("Can't You See Me?", "TOMORROW X TOGETHER", "03:21"));
+            tracks.Add(new Track("Eternally", "TOMORROW X TOGETHER", "03:37"));
+            tracks.Add(new Track("Spark", "JBJ95", "03.36"));
+            tracks.Add(new Track("TO BE OR NOT TO BE", "Oneus", "03.48"));
+            tracks.Add(new Track("Yakusoku", "GARNiDELiA", "04.16"));
+            tracks.Add(new Track("Touch off", "UVERworld", "04.12"));
 
-            tracks[0].name = "Drama";
-            tracks[0].artist = "TOMORROW X TOGETHER";
-            tracks[0].time = "3:29";
-            tracks[1].name = "Can't You See Me?";
-            tracks[1].artist = "TOMORROW X TOGETHER";
-            tracks[1].time = "3:21";
-            tracks[2].name = "Fairy of Shampoo";
-            tracks[2].artist = "TOMORROW X TOGETHER";
-            tracks[2].time = "4:27";
-            tracks[3].name = "Maze in the Mirror";
-            tracks[3].artist = "TOMORROW X TOGETHER";
-            tracks[3].time = "3:46";
-            tracks[4].name = "PUMA";
-            tracks[4].artist = "TOMORROW X TOGETHER";
-            tracks[4].time = "3:25";
-            tracks[5].name = "Eternally";
-            tracks[5].artist = "TOMORROW X TOGETHER";
-            tracks[5].time = "3:37";
-            Tracklist = tracks.Length;
+            Tracklist = tracks.Count;
         }
         public Track PlayCD(int order)
         {
             Tracklist--;
             return tracks[order];
+        }
+        public void Addmusic(string name, string artist, string time)
+        {
+            tracks.Add(new Track(name, artist, time));
+        }
+        public void Show()
+        {
+            foreach (var t in tracks)
+            {
+                Console.Write("         " + t.name);
+                Console.Write(" --- " + t.artist);
+                Console.WriteLine(" --- " + t.time);
+            }
         }
     }
 }
