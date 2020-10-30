d4d10533e3689f697e973ff0cc39e5d8c332a9fb
suchon <57866286+Suchon0544@users.noreply.github.com>
2020-10-30 12:36:13 +0700

    Update and rename player.cs to botton.cs

--- MediaPlayer/botton.cs
+++ MediaPlayer/botton.cs
@@ -1,42 +1,42 @@
 using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
 
 namespace MediaPlayer
 {
-    public class Bottom
+    public class Botton
     {
         Album album1 = new Album();
-        int _next = 0;
+        int ordering = 0;
 
+        public void Display()
+        {
+            Console.WriteLine("Now Playing   :  " + album1.PlayCD(ordering).name);
+            Console.WriteLine("Artist   :  " + album1.PlayCD(ordering).artist);
+            Console.WriteLine(" 00.00 ------------------------- " + album1.PlayCD(ordering).time);
+        }
         public void play()
         {
-            Console.WriteLine("Now Playing   :  " + album1.PlayCD(_next).name);
-            Console.WriteLine("Artist   :  " + album1.PlayCD(_next).artist);
-            Console.WriteLine(" 00.00 ------------------------- " + album1.PlayCD(_next).time);
+            Display();
             Console.WriteLine("<              [pause]              >");
         }
 
         public void next()
         {
-            _next = _next + 1;
-            Console.WriteLine("Now Playing   :  " + album1.PlayCD(_next).name);
-            Console.WriteLine("Artist   :  " + album1.PlayCD(_next).artist);
-            Console.WriteLine(" 00.00 ------------------------- " + album1.PlayCD(_next).time);
+            ordering = ordering + 1;
+            Display();
             Console.WriteLine("<              [pause]              >");
         }
         public void previous()
         {
-            _next = _next - 1;
-            Console.WriteLine("Now Playing   :  " + album1.PlayCD(_next).name);
-            Console.WriteLine("Artist   :  " + album1.PlayCD(_next).artist);
-            Console.WriteLine(" 00.00 ------------------------- " + album1.PlayCD(_next).time);
+            ordering = ordering - 1;
+            Display();
             Console.WriteLine("<              [pause]              >");
         }
         public void Stop()
         {
-            Console.WriteLine("Now Playing   :  " + album1.PlayCD(0).name);
-            Console.WriteLine("Artist   :  " + album1.PlayCD(0).artist);
-            Console.WriteLine(" 00.00 ------------------------- " + album1.PlayCD(0).time);
+            ordering = 0;
+            Display();
             Console.WriteLine("<               [play]               >");
         }
 
@@ -52,11 +52,11 @@ namespace MediaPlayer
         }
         public static void ask()
         {
-            Bottom bottom = new Bottom();
+            Botton botton = new Botton();
             Album album = new Album();
         UP:
             Console.WriteLine("     ");
-            Console.Write(" Input for press bottom : ");
+            Console.Write(" Input for press botton : ");
             string Answer = Console.ReadLine();
             Console.WriteLine("        ");
             //Console.WriteLine("     " + Answer);
@@ -64,27 +64,27 @@ namespace MediaPlayer
             switch (Answer)
             {
                 case "p":
-                    bottom.play();
+                    botton.play();
                     goto UP;
                     break;
                 case "n":
-                    bottom.next();
+                    botton.next();
                     goto UP;
                     break;
                 case "r":
-                    bottom.previous();
+                    botton.previous();
                     goto UP;
                     break;
                 case "s":
-                    bottom.Stop();
+                    botton.Stop();
                     goto UP;
                     break;
                 case "a":
-                    bottom.add();
+                    botton.add();
                     goto UP;
                     break;
                 case "q":
-                    album.Show();
+                    album.show();
                     goto UP;
                     break;
                 case "e":
