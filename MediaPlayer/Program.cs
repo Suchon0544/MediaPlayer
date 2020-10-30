b3fabecc4b7dee4fa006e1ecf5fe5ad51c7f7aaf
suchon <57866286+Suchon0544@users.noreply.github.com>
2020-10-30 10:42:38 +0700

    Update Program.cs

--- MediaPlayer/Program.cs
+++ MediaPlayer/Program.cs
@@ -16,24 +16,23 @@ namespace MediaPlayer
         
         static void Main(string[] args)
         {
-            Track t1 = new Track();
             Album album1 = new Album();
             Bottom bottom1 = new Bottom();
 
-            Console.WriteLine("Input: 1 for play your music");
-            Console.WriteLine("       2 for play next music");
-            Console.WriteLine("       3 for play previous music");
-            Console.WriteLine("       4 for stop playing");
-            Console.WriteLine("       0 for add music");
+            Console.WriteLine("Input: p for play your music");
+            Console.WriteLine("       n for play next music");
+            Console.WriteLine("       r for play previous music");
+            Console.WriteLine("       s for stop playing");
+            Console.WriteLine("       a for add music");
+            Console.WriteLine("       q for show tracklist");
+            Console.WriteLine("       e for close");
             Console.WriteLine("     ");
-            Console.WriteLine("Now Playing   :  "+ album1.PlayCD(1).name);
-            Console.WriteLine("Artist   :  "+ album1.PlayCD(1).artist);
-            Console.WriteLine(" 00.00 ------------------------- " + album1.PlayCD(1).time);
+            Console.WriteLine("Now Playing   :  --");
+            Console.WriteLine("Artist   :  --");
+            Console.WriteLine(" --.-- ------------------------- --.--");
             Console.WriteLine("<               [play]               >");
-            for (int i = 0; i < 10; i++)
-            {
-                Bottom.ask();
-            }
+
+            Bottom.ask();
         }
     }
 }    
